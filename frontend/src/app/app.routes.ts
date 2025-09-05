9df4d20285e3ed736a7f8032cb1060efcb9ab8a4
Atom <atomclem@gmail.com>
2025-09-05 22:01:18 +0200

    refractor: routes update

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { AuthComponent } from './auth/auth.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectCreateComponent } from './project/project-create.component';
 import { ProjectViewComponent } from './project/project-view.component';
@@ -8,8 +7,7 @@ import { ProjectListComponent } from './project/project-list.component';
 import { AuthGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: 'auth', component: AuthComponent },
   { path: 'home', component: HomeComponent},
   { path: 'project', component: ProjectCreateComponent, canActivate: [AuthGuard] },
   { path: 'projects', component: ProjectListComponent, canActivate: [AuthGuard] }, // liste des projets
