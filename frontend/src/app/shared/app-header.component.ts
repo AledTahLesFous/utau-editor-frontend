8348f66a39c7d91d69247fc87452b7f03c63e2bc
Atom <atomclem@gmail.com>
2025-09-06 00:22:07 +0200

    feat: header component

---
+++ frontend/src/app/shared/app-header.component.ts
@@ -0,0 +1,40 @@
+// app-header.component.ts
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app-header.component.html',
+})
+export class AppHeaderComponent {
+  @Input() isLoggedIn = !!localStorage.getItem('token');
+
+  constructor(private router: Router) {}
+
+  goToLogin() {
+    this.router.navigate(['/auth']);
+  }
+
+  goToProfile() {
+    this.router.navigate(['/profile']);
+  }
+
+  goToCreateProjet() {
+    if (!this.isLoggedIn) {
+      this.router.navigate(['/auth']);
+    } else {
+      this.router.navigate(['/project']);
+    }
+  }
+
+  goToViewProjects() {
+    if (!this.isLoggedIn) {
+      this.router.navigate(['/auth']);
+    } else {
+      this.router.navigate(['/projects']);
+    }
+  }
+}
