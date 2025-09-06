20f0decb90a6fd0c1f21e61fef92ab18cb1e4de3
Atom <atomclem@gmail.com>
2025-09-06 16:24:25 +0200

    fix: route update new route for view and edit

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { HomeComponent } from './home/home.component';
 import { ProjectCreateComponent } from './project/project-create.component';
 import { ProjectViewComponent } from './project/project-view.component';
 import { ProjectListComponent } from './project/project-list.component';
+import { ProjectEditComponent } from './project/project-edit.component';
+
 import { AuthGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
@@ -11,7 +13,8 @@ export const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'project', component: ProjectCreateComponent, canActivate: [AuthGuard] },
   { path: 'projects', component: ProjectListComponent, canActivate: [AuthGuard] }, // liste des projets
-  { path: 'project/:name', component: ProjectViewComponent, canActivate: [AuthGuard] },
+  { path: 'project/:name', component: ProjectEditComponent, canActivate: [AuthGuard] },
+  { path: 'project-view/:name', component: ProjectViewComponent }, // public
   { path: '', component: HomeComponent},
   { path: '**', redirectTo: '' }
 ];
