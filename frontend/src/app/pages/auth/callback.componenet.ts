48bdf8cb52ec082340d7b993546636f003119aa6
Atom <clement.muzard@etu.umontpellier.fr>
2025-10-28 16:13:57 +0100

    chore: changes for oauth but still not working

--- frontend/src/app/pages/auth/callback.componenet.ts
+++ frontend/src/app/pages/auth/callback.componenet.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -10,17 +9,17 @@ import { CommonModule } from '@angular/common';
   template: `<p>Connexion en cours via GitHub...</p>`,
 })
 export class AuthCallbackComponent implements OnInit {
-  constructor(private router: Router, private http: HttpClient) {}
+  constructor(private router: Router) {}
 
-ngOnInit() {
-  this.http.get('http://localhost:8055/auth/exchange-cookie', { withCredentials: true })
-    .subscribe({
-      next: (res: any) => {
-        localStorage.setItem('token', res.access_token);
-        this.router.navigate(['/home']);
-      },
-      error: (err) => console.error('Erreur échange cookie → JWT', err),
-    });
-}
+  ngOnInit() {
+    const params = new URLSearchParams(window.location.search);
+    const token = params.get('access_token'); // Directus renvoie le JWT
 
+    if (token) {
+      localStorage.setItem('token', token);
+      this.router.navigate(['/home']);
+    } else {
+      console.error('Token Directus manquant');
+    }
+  }
 }
